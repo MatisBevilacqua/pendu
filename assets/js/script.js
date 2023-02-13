76166f557995eb92220521380ef6cc88a0577775
enioaiello <eaiello@edenschool.fr>
2023-02-13 15:31:56 +0100

    Mise à jour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,41 @@
 let result = document.querySelector("#content");
 let content = document.querySelector("input[type=text]");
 let submit = document.querySelector("input[type=submit]");
+let word = ["VOITURE", "FRUIT", "JEU", "NATURE"];
+let createSpan = null;
 
 function check(e) {
     e.preventDefault();
     if (content.value.length < 2) {
-         console.log("Ok");
+         
     } else {
          console.log("...");
     }
     console.log(content.value.length)
 }
 
+function randomize(tab) {
+    let i, j, tmp;
+    for (i = tab.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        tmp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = tmp;
+    }
+    return tab;
+}
+
+let game = randomize(word);
+let resultEnd = game[0];
+
+function selectWord() {
+    for (let i = 0; i > resultEnd.length; i++) {
+        createSpan = document.createElement("span");
+        result.appendChild(createSpan);
+        console.log(createSpan);
+    }
+}
+
+console.log(resultEnd.length);
+selectWord();
 submit.addEventListener("click", check);
