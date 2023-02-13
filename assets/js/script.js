a9bc153a5d9e6bf822bb94e7b19b887148110f50
enioaiello <eaiello@edenschool.fr>
2023-02-13 11:34:10 +0100

    Mise à jour du JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,12 @@ let submit = document.querySelector("input[type=submit]");
 
 function check(e) {
     e.preventDefault();
-    if (content.value > 1) {
+    if (content.value.length < 2) {
          console.log("Ok");
     } else {
          console.log("...");
     }
+    console.log(content.value.length)
 }
 
-addEventListener("click", check);
+submit.addEventListener("click", check);
